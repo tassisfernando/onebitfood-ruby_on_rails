0b762b733fe90982f417fdd3cbcea09c1f4ce28c
Tássis Fernando <tassis_fernando@hotmail.com>
2019-05-03 23:52:27 -0300

    Finalizando Api

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   def create
     order = Order.new(order_params)
     if order.save
-      render json: @order, status: :created
+      render json: order, status: :created
     else
       render json: order.errors, status: :unprocessable_entity
     end
